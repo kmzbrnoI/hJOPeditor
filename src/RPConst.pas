2197375e474720fd4e04cb7b4e409c736e38a578
Jan Horacek <me@apophis.cz>
2018-07-30 17:42:18 +0200

    Sirka souprav v trati je 9.

--- src/RPConst.pas
+++ src/RPConst.pas
@@ -13,7 +13,7 @@ const
  //zde jsou definovany jednotluve barvy
  _Symbol_Colors: array [0..(_Symbol_ColorsCount-1)] of TColor = ($FF00FF,$A0A0A0,$0000FF,$00FF00,$FFFFFF,$FFFF00,$FF0000,$00FFFF,$800080,$000000);
 
- _Uvazka_Spr_Sirka = 8;
+ _Uvazka_Spr_Sirka = 9;
 
  _BLK_VYH     = 0;
  _BLK_USEK    = 1;
