7b2f943aba2a39a712715d29da6e10e85427e96d
Jan Horacek <me@apophis.cz>
2020-03-11 12:30:37 +0100

    Podporovat opnl v1.3.

--- src/object/blok/ObjBlokPrejezd.pas
+++ src/object/blok/ObjBlokPrejezd.pas
@@ -18,13 +18,15 @@ TPrejezd = class(TGraphBlok)
 
  constructor Create(index:Integer);
  destructor Destroy(); override;
- procedure Load(ini:TMemIniFile; key:string; version: string); override;
+ procedure Load(ini:TMemIniFile; key:string; version: Word); override;
  procedure Save(ini:TMemIniFile; key:string); override;
  procedure Paint(DrawObject:TDrawObject; panelGraphics:TPanelGraphics; colors:TObjColors;
                   selected:boolean; mode:TMode); override;
 end;
 
 implementation
+
+uses ReliefObjects;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -45,22 +47,30 @@ end;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-procedure TPrejezd.Load(ini:TMemIniFile; key:string; version: string);
+procedure TPrejezd.Load(ini:TMemIniFile; key:string; version: Word);
 var obj:string;
     pos:TPoint;
     j:Integer;
     blik_pos:TBlikPoint;
+    count, section_len: Integer;
 begin
  inherited;
 
+ if (version >= _FILEVERSION_13) then
+   section_len := 16
+ else
+   section_len := 9;
+
  obj := ini.ReadString(key, 'BP', '');
  Self.BlikPositions.Clear();
- for j := 0 to (Length(obj) div 9)-1 do
+
+ count := (Length(obj) div section_len);
+ for j := 0 to count-1 do
   begin
    try
-     blik_pos.Pos.X    := StrToInt(copy(obj, j*9+1, 3));
-     blik_pos.Pos.Y    := StrToInt(copy(obj, j*9+4, 3));
-     blik_pos.TechUsek := StrToInt(copy(obj, j*9+7, 3));
+     blik_pos.Pos.X    := StrToInt(copy(obj, j*section_len+1, 3));
+     blik_pos.Pos.Y    := StrToInt(copy(obj, j*section_len+4, 3));
+     blik_pos.TechUsek := StrToInt(copy(obj, j*section_len+7, 3));
    except
      continue;
    end;
@@ -93,9 +103,9 @@ begin
  for bp in Self.BlikPositions do
   begin
    if (bp.TechUsek >= 0) then
-     obj := obj + Format('%.3d%.3d%.3d',[bp.Pos.X, bp.Pos.Y, bp.TechUsek])
+     obj := obj + Format('%.3d%.3d%.10d',[bp.Pos.X, bp.Pos.Y, bp.TechUsek])
    else
-     obj := obj + Format('%.3d%.3d-01',[bp.Pos.X, bp.Pos.Y]);
+     obj := obj + Format('%.3d%.3d-000000001',[bp.Pos.X, bp.Pos.Y]);
   end;
  ini.WriteString(key, 'BP', obj);
 
